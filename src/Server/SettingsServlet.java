f03d2c569d52e1c5bdbc0bd2cd5d5b104668b36b
Boshra Nabaei <b.nabaei@gmail.com>
2017-02-15 20:59:03 -0800

    minor modifcations

--- src/Server/SettingsServlet.java
+++ src/Server/SettingsServlet.java
@@ -13,7 +13,7 @@ public class SettingsServlet extends HttpServlet {
 	public void init() throws ServletException {
 	}
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		String username = request.getParameter("username");
 		String oldPassword = request.getParameter("oldPassword");
 		String newPassword = request.getParameter("newPassword");
@@ -26,4 +26,5 @@ public class SettingsServlet extends HttpServlet {
 		}
 		out.close();
 	}
+	
 }
